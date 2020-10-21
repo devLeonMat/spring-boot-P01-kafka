ab86e343957c1525e7555db9b2d75b49bf09a156
Leon Matias Roslin <leonmatias1991@gmail.com>
2020-10-21 00:30:58 -0500

    add first version

---
+++ src/main/java/com/rleon/examplekafkaP01/ExampleKafkaP01Application.java
@@ -0,0 +1,13 @@
+package com.rleon.examplekafkaP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleKafkaP01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleKafkaP01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/examplekafkaP01/ExampleKafkaP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.examplekafkaP01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleKafkaP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
